1550cfaae42fd4897b1e8a18e8561481837ae4ae
thanos <chaddwick25@yahoo.ca>
2022-05-30 20:57:54 -0400

    completed 112 : Specifying the Runtime Environment

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,26 +1,46 @@
 import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { Config } from 'prettier';
 const cookieSession: any = require('cookie-session');
 
 
 @Module({
   //TypeOrm will create a 'db.sqlite' file within the root dir
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    // non NestJs solution
-    // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-    entities:[User, Report],
-    synchronize: true,
-
-  }),UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type:'sqlite',
+          database: config.get<string>('DB_NAME'), 
+          entities: [User, Report],
+          synchronize:true,
+        }
+      } 
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+      // // non NestJs solution
+      // // database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
+    //   entities:[User, Report],
+    //   synchronize: true,    
+    // }),
+    UsersModule,
+    ReportsModule
+  ],
   controllers: [AppController],
   // globally scoped validation pipe 
   providers: [
