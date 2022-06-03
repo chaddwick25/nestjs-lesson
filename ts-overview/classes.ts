036c1a6ef22b37f1575df11202a07a12f72f0cee
thanos <chaddwick25@yahoo.ca>
2022-06-03 07:24:43 -0400

    completed : 204. Class Modifiers

--- ts-overview/classes.ts
+++ ts-overview/classes.ts
@@ -1,19 +1,24 @@
 class Vehicle {
-    drive(): void {
-        console.log('chugga chugga')
-    }
-    honk(): void {
+    // public drive(): void {
+    //     console.log('chugga chugga')
+    // }
+    protected honk(): void {
         console.log('beep')
     }
 }
 
 //override drive method
 class Car extends Vehicle {
-    drive(): void {
+    private drive(): void {
         console.log('vrom')
+    }
+
+    startDrivingProcess(): void {
+        this.drive();
+        this.honk();
     }
 }
 
 const car = new Car();
-car.drive()
-car.honk()
+car.startDrivingProcess()
+// car.honk()
